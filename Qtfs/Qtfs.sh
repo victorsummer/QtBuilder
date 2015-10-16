67a3331033aa9bea8850e147a0c82166215071c4
Vito <victorsummer@gmail.com>
2015-10-16 14:12:07 +0800

    Update Qtfs.sh

--- Qtfs/Qtfs.sh
+++ Qtfs/Qtfs.sh
@@ -28,5 +28,7 @@ cd ..
 rm -rdf qt-everywhere-opensource-src-5.3.2
 rm qt-everywhere-opensource-src-5.3.2.tar.gz
 
+sudo rm -rdf $HOME/Qtfs
+
 echo "Done! The compiled Qt is installed at $HOME/Qtfs"
 pause
