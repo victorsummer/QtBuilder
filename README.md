4dcc1fb44ac8e951bb1eb93b48c45264728f7ae9
vito <victorsummer@gmail.com>
2015-12-16 10:01:27 +0800

    Add Linux static compilation

--- README.md
+++ README.md
@@ -16,3 +16,18 @@ Just double click StaticQtMingW.bat, and that is all.
 
  1. It does not support webkit at the moment.
  2. It is tested with Qt5.4.1 only.
+
+# Qtfs
+Compile Qt5.5.1 for freescale imx6
+
+# StaticQtLinux
+Compile static Qt on Linux
+
+* Requirement:
+
+You need to have access to the internet, because the build script will update the system and install necessary dependencies. 
+
+* Limit:
+
+ 1. It is tested with Qt5.6.0 enterprise alpha only.
+ 2. Only tested on Ubuntu 14.04 64 bits
---
+++ StaticQtLinux/StaticQt5.6a.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+CURRENTPATH=$PWD
+
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get install -y build-essential
+sudo apt-get install -y python
+sudo apt-get install -y perl
+
+tar xvfz qt-everywhere-enterprise-src-5.6.0-alpha.tar.gz
+cd qt-everywhere-enterprise-src-5.6.0-alpha
+
+echo "Configuring Qt"
+./configure -prefix $HOME/StaticQt5.6.0Alpha -static -confirm-license -qt-xcb -nomake examples -nomake tests
+
+echo "Compiling Qt"
+rm -rf $HOME/StaticQt5.6.0Alpha
+make -j4
+make install
+
+cd $CURRENTPATH
+rm -rf qt-everywhere-enterprise-src-5.6.0-alpha
+
+echo "Done! The compiled static Qt is installed at $HOME/StaticQt5.6.0Alpha"
